6597fe6874af0461bba5fd541849f3751ec80238
Swetika <116952677+Vidushi23@users.noreply.github.com>
2023-01-12 16:24:02 +0530

    Added files

---
+++ index.js
@@ -0,0 +1,58 @@
+var i=0;
+for(i=0;i<7;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",gotClick);
+}
+function gotClick(){
+    
+    
+    var btnPress=this.innerHTML;
+    makeSound(btnPress);
+    buttonAnimation(btnPress);
+}
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    switch(key){
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default: console.log(btnPress);   
+    }
+}
+function buttonAnimation(curKey){
+    var activeBtn= document.querySelector("."+ curKey);
+    
+    activeBtn.classList.add("pressed");
+    setTimeout(function(){
+        activeBtn.classList.remove("pressed");
+    },100);
+}
